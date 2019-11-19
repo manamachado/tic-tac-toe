7331b949a38510c3486bc3222b2404a7ee490d4d
Maná Machado <manasses.machado@luizalabs.com>
2019-11-19 10:58:41 -0300

    Updates

---
+++ precache-manifest.db5fd895ddf3eca4dc428e241ade5079.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f16b5520f96daa4dd93ced4d1e820be",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c8a6049666e43a1f4992",
+    "url": "/tic-tac-toe/static/css/main.ef45ab39.chunk.css"
+  },
+  {
+    "revision": "4b6016b2fb834729c3c4",
+    "url": "/tic-tac-toe/static/js/2.2fb698b7.chunk.js"
+  },
+  {
+    "revision": "c8a6049666e43a1f4992",
+    "url": "/tic-tac-toe/static/js/main.70993a7d.chunk.js"
+  },
+  {
+    "revision": "25d94bd95813189daa86",
+    "url": "/tic-tac-toe/static/js/runtime-main.00b9c82d.js"
+  }
+]);
